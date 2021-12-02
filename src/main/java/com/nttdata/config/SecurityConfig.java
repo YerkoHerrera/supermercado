186c391b18a77303df5ef1e9d1279d1e45d10b85
Yerko Herrera Alarcon <yherrala@emeal.nttdata.com>
2021-12-02 20:29:18 -0300

    Se agrego una rama de seguridad

---
+++ src/main/java/com/nttdata/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.nttdata.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.nttdata.services.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/","/login/**","/public/**","/css/**","/js/**").permitAll().anyRequest().authenticated()
+		.and()
+			.formLogin().loginPage("/login").defaultSuccessUrl("/login",true).failureUrl("/login?error=true")
+			.loginProcessingUrl("/registro").permitAll()
+		.and()
+			.logout().logoutUrl("/logout").logoutSuccessUrl("/public/index");
+	}
+
+	
+	
+	
+	
+	
+	
+}
